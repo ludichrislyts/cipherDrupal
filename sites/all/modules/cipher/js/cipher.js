30ee69a43e58ab09bdb421cdd2b7438269f9bb70
JackMackerel <ludichrislyts@gmail.com>
2015-10-23 20:42:08 -0700

    get ajax to work letter by letter

--- sites/all/modules/cipher/js/cipher.js
+++ sites/all/modules/cipher/js/cipher.js
@@ -2,23 +2,44 @@
 {
 	Drupal.ajax.prototype.commands.validateDirectionCallback = function(ajax, response, status)
 	{
+		//console.log(input);
 		var error_div = document.getElementById('error_message');
 		var input = error_div.getElementsByTagName("INPUT")[0];
-		//console.log(input);
-		var entry = String(response.selectedValue);
-		var errorDiv = document.createElement("P");
-		$(errorDiv).css('color', 'red');
-		var errorText = document.createTextNode("Entry must be either 'left' or 'right'");
-		errorDiv.appendChild(errorText);
-		
+		var entry = String(response.selectedValue);		
 		var len = response.selectedValue.length;
 		var required = ['left', 'right'];
 		for(var i = 0; i < len; i++){
-			console.log(entry[0] + ', ' + required[1][i]);
-			if(entry[i] === required[0][i] || entry[i] === required[1][i]){
+			if(entry.length === required[1].length){ 
+				if(entry === required[1]){
+					console.log(entry + ', ' + required[0] + ', ' + required[1] + ', nested if');
+					continue;					
+				}else{
+					console.log('it is here in second else');
+					var errorP = document.createElement("P");
+					$(errorP).css('color', 'red');
+					var errorText = document.createTextNode("Entry must be either 'left' or 'right'");
+					errorP.appendChild(errorText);
+					document.getElementById('error_message').appendChild(errorP);
+				}
+			}else if(entry[i] === required[0][i] || entry[i] === required[1][i] ){
+				console.log(entry[i] + ', ' + required[0][i] + ", " + required[1][i] + ', main if loop');
 				continue;
+				// }
+				// else{
+				// 	console.log(entry + ', it is here in first else');
+				// 	var errorP = document.createElement("P");
+				// 	$(errorP).css('color', 'red');
+				// 	var errorText = document.createTextNode("Entry must be either 'left' or 'right'");
+				// 	errorP.appendChild(errorText);
+				// 	document.getElementById('error_message').appendChild(errorP);	
+				// }
 			}else{
-				document.getElementById('error_message').appendChild(errorDiv);
+				console.log('it is here in second else');
+				var errorP = document.createElement("P");
+				$(errorP).css('color', 'red');
+				var errorText = document.createTextNode("Entry must be either 'left' or 'right'");
+				errorP.appendChild(errorText);
+				document.getElementById('error_message').appendChild(errorP);
 		// if(entry.length === 4 && entry !== 'left' && entry !== 'righ'){
 			// var errorDiv = document.createElement("DIV");
 			// errorDiv.appendChild(errorText);
